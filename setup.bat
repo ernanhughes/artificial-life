ef2ebd5ce0da792f9cef9a50a1b50a1abe2981b5
Ernan Hughes <155574808+ernanhughes@users.noreply.github.com>
2024-10-22 23:34:04 +0100

    Added rule 39 implementation

---
+++ setup.bat
@@ -0,0 +1,5 @@
+python -m venv .envWhen his head was start
+start PowerShell.exe -NoExit -Command .\.env\Scripts\Activate.ps1
+python.exe -m pip install --upgrade pip
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r .\requirements.txt
